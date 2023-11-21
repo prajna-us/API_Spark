5d5b150dfc27b29ec9e6fc0068be27fb8dd79a72
Shubhangi Kulkarni <108959166+shubhangiskulkarni@users.noreply.github.com>
2023-11-21 01:38:15 -0600

    All controllers

---
+++ src/test/java/stepDifinitions/MorbiditySD.java
@@ -0,0 +1,161 @@
+package stepDifinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import utilities.LoggerLoad;
+import utilities.ReadJson;
+import utilities.ReadProperties;
+
+import static org.junit.Assert.assertEquals;
+
+public class MorbiditySD {
+
+
+    private String bearerToken;
+
+    private RequestSpecification requestSpec;
+
+    private final ReadJson readJson = new ReadJson();
+
+    private Response response;
+
+
+    @Given("the user has set Authorization to Bearer Token")
+    public void the_user_has_set_authorization_to_bearer_token() {
+
+        //log file entry
+        LoggerLoad.info("Reading token...");
+        ReadProperties.loadConfig();
+
+        //reading token from properties file.
+        bearerToken = ReadProperties.getProperty("bearerToken");
+        System.out.println(bearerToken);
+
+    }
+
+    @When("User creates a GET https request for the API endpoint to get all morbidities")
+    public void user_creates_a_get_https_request_for_the_api_endpoint_to_get_all_morbidities() {
+
+        //log file entry
+        LoggerLoad.info("Reading GET endpoint...");
+
+        //reading properties file.
+        RestAssured.baseURI = ReadProperties.getProperty("baseURL");
+
+        //reading GET endpoint from properties file
+        String endpoint = ReadProperties.getProperty("Get_All_Morbidity");
+
+        System.out.println(endpoint);
+
+        //log file entry
+        LoggerLoad.info("Get all Morbidity...");
+
+        //Making the GET call by passing the auth token
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + bearerToken)
+                .get(endpoint);
+
+
+        // Log the response body
+        response.body().print();
+
+
+    }
+
+    @Then("Response status code should be {int} OK with response body")
+    public void response_status_code_should_be_ok_with_response_body(int expectedStatusCode) {
+
+
+        //log file entry
+        LoggerLoad.info("Asserting response status code...");
+
+        //Asserting 200 status code
+        assertEquals(expectedStatusCode, response.getStatusCode());
+
+        // Log the response body
+        response.body().print();
+
+    }
+
+    @When("User creates GET request for the  morbidity API endpoint")
+    public void user_creates_get_request_for_the_morbidity_api_endpoint() {
+
+        //log file entry
+        LoggerLoad.info("Reading GET endpoint...");
+
+        //reading properties file.
+        RestAssured.baseURI = ReadProperties.getProperty("baseURL");
+
+        //reading GET endpoint from properties file
+        String endpoint = ReadProperties.getProperty("Get_Morbidity_By_FastingGlucose");
+
+        System.out.println(endpoint);
+
+        //log file entry
+        LoggerLoad.info("Get all Morbidity for Fasting Glucose...");
+
+        //Making the GET call by passing the auth token
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + bearerToken)
+                .get(endpoint);
+
+
+        // Log the response body
+        response.body().print();
+
+
+    }
+
+
+    @When("User creates GET request for the  morbidity API endpoint with invalid name")
+    public void invalidName() {
+
+
+        //log file entry
+        LoggerLoad.info("Reading GET endpoint...");
+
+        //reading properties file.
+        RestAssured.baseURI = ReadProperties.getProperty("baseURL");
+
+        //reading GET endpoint from properties file
+        String endpoint = ReadProperties.getProperty("Get_Morbidity_By_InvalidName");
+
+        System.out.println(endpoint);
+
+        //log file entry
+        LoggerLoad.info("Get all Morbidity for Fasting Glucose...");
+
+        //Making the GET call by passing the auth token
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + bearerToken)
+                .get(endpoint);
+
+
+        // Log the response body
+        response.body().print();
+
+    }
+
+
+    @Then("Response status code should be {int}  with response body")
+    public void response_status_code_should_be_with_response_body(int expectedStatusCode) {
+
+
+        //log file entry
+        LoggerLoad.info("Asserting response status code...");
+
+        //Asserting 200 status code
+        assertEquals(expectedStatusCode, response.getStatusCode());
+
+        // Log the response body
+        response.body().print();
+
+    }
+
+
+
+}
--- src/test/java/stepDifinitions/UserSD.java
+++ src/test/java/stepDifinitions/UserSD.java
@@ -4,35 +4,93 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 import utilities.LoggerLoad;
+import utilities.ReadJson;
 import utilities.ReadProperties;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserSD {
 
-    private Response response;
     private String bearerToken;
 
-    //	@Given ("User sets Authoization to Bearer Token")
-//	public void user_sets_authoization_to_bearer_token () {
-//
-//		bearerToken = ReadProperties.getProperty("bearerToken");
-//
-//
-//	}
+    private RequestSpecification requestSpec;
+
+    private final ReadJson readJson = new ReadJson();
+
+    private Response response;
+
+    	@Given ("User sets Authoization to Bearer Token for login")
+	public void user_sets_authoization_to_bearer_token_for_login () {
+
+            LoggerLoad.info("Reading token...");
+            ReadProperties.loadConfig();
+            bearerToken = ReadProperties.getProperty("bearerToken");
+
+
+	}
     @Given("User creates request for the userlogin API endpoint")
     public void user_creates_request_for_the_userlogin_API_endpoint() {
 
+        //log file entry
+        LoggerLoad.info("Setting baseURI for RestAssured...");
+
+        //reading properties file.
+        RestAssured.baseURI = ReadProperties.getProperty("baseURL");
+
+        requestSpec = RestAssured.given()
+                .header("Authorization", "Bearer " + bearerToken)
+                .header("accept", "application/json")
+                .header("Content-Type", ContentType.JSON);
 
     }
 
     @When("User sends HTTPS Request and  request Body with Required Field")
     public void user_sends_HTTPS_Request_and_request_Body_with_Required_Field() {
 
+        //log file entry
+        LoggerLoad.info("Reading POST endpoint...");
+
+        String endpoint = ReadProperties.getProperty("user_PostEndpoint");
+
+        LoggerLoad.info("Making POST call for patient login...");
+
+
+        //Reading test data
+        JSONObject testData = readJson.setUpTestData();
+        JSONObject patientInfo = testData.getJSONObject(ReadProperties.getProperty("PatientLogin"));
+
+        // Convert patientInfo to JSON string
+        String patientInfoJson = "{" +
+                "\"password\":\"" + patientInfo.getString("password") + "\"," +
+                "\"userLoginEmail\":\"" + patientInfo.getString("userLoginEmail") + "\"" +
+                "}";
+
+        System.out.println(patientInfoJson);
+
+        // Send the POST request with form parameters
+        response = requestSpec
+                .body(patientInfoJson)
+                .post(endpoint);
+
+        // Log the response body
+        response.body().print();
+
     }
 
     @Then("User receives {int} Created Status with response body")
-    public void user_receives_Created_Status_with_response_body(Integer int1) {
+    public void user_receives_Created_Status_with_response_body(int expectedStatusCode) {
+
+
+        LoggerLoad.info("Asserting response status code...");
+        assertEquals(expectedStatusCode, response.getStatusCode());
+
 
     }
 
@@ -40,10 +98,41 @@ public class UserSD {
     @When("User sends HTTPS Request and Body with valid password and invalid email")
     public void user_sends_HTTPS_Request_and_Body_with_valid_password_and_invalid_email() {
 
+        //log file entry
+        LoggerLoad.info("Reading POST endpoint...");
+
+        String endpoint = ReadProperties.getProperty("user_PostEndpoint");
+
+        LoggerLoad.info("Making POST call for patient login...");
+
+
+        //Reading test data
+        JSONObject testData = readJson.setUpTestData();
+        JSONObject patientInfo = testData.getJSONObject(ReadProperties.getProperty("PatientLogin"));
+
+        // Convert patientInfo to JSON string
+        String patientInfoJson = "{" +
+                "\"password\":\"" + patientInfo.getString("password") + "\"," +
+                "\"userLoginEmail\":\"" + "stringgmail.com" + "\"" +
+                "}";
+
+        System.out.println(patientInfoJson);
+
+        // Send the POST request with form parameters
+        response = requestSpec
+                .body(patientInfoJson)
+                .post(endpoint);
+
+        // Log the response body
+        response.body().print();
+
     }
 
     @Then("User receives {int} Bad Request Status with message body")
-    public void user_receives_Bad_Request_Status_with_message_body(Integer int1) {
+    public void user_receives_Bad_Request_Status_with_message_body(int expectedStatusCode) {
+
+        LoggerLoad.info("Asserting response status code...");
+        assertEquals(expectedStatusCode, response.getStatusCode());
 
     }
 
@@ -51,11 +140,68 @@ public class UserSD {
     @When("User sends HTTPS Request and  request Body with invalid password and valid email")
     public void user_sends_HTTPS_Request_and_request_Body_with_invalid_password_and_valid_email() {
 
+//log file entry
+        LoggerLoad.info("Reading POST endpoint...");
+
+        String endpoint = ReadProperties.getProperty("user_PostEndpoint");
+
+        LoggerLoad.info("Making POST call for patient login...");
+
+
+        //Reading test data
+        JSONObject testData = readJson.setUpTestData();
+        JSONObject patientInfo = testData.getJSONObject(ReadProperties.getProperty("PatientLogin"));
+
+        // Convert patientInfo to JSON string
+        String patientInfoJson = "{" +
+                "\"password\":\"" + "test1" + "\"," +
+                "\"userLoginEmail\":\"" + patientInfo.getString("userLoginEmail") + "\"" +
+                "}";
+
+        System.out.println(patientInfoJson);
+
+        // Send the POST request with form parameters
+        response = requestSpec
+                .body(patientInfoJson)
+                .post(endpoint);
+
+        // Log the response body
+        response.body().print();
+
     }
 
 
     @When("User sends HTTPS Request and  request Body with invalid password and invalid email")
     public void user_sends_HTTPS_Request_and_request_Body_with_invalid_password_and_invalid_email() {
+
+
+//log file entry
+        LoggerLoad.info("Reading POST endpoint...");
+
+        String endpoint = ReadProperties.getProperty("user_PostEndpoint");
+
+        LoggerLoad.info("Making POST call for patient login...");
+
+
+        //Reading test data
+        JSONObject testData = readJson.setUpTestData();
+        JSONObject patientInfo = testData.getJSONObject(ReadProperties.getProperty("PatientLogin"));
+
+        // Convert patientInfo to JSON string
+        String patientInfoJson = "{" +
+                "\"password\":\"" + "test1" + "\"," +
+                "\"userLoginEmail\":\"" + "stringgamil.com" + "\"" +
+                "}";
+
+        System.out.println(patientInfoJson);
+
+        // Send the POST request with form parameters
+        response = requestSpec
+                .body(patientInfoJson)
+                .post(endpoint);
+
+        // Log the response body
+        response.body().print();
 
     }
 
@@ -63,18 +209,77 @@ public class UserSD {
     @When("User sends HTTPS Request and request Body with missing mandatory fields")
     public void user_sends_HTTPS_Request_and_request_Body_with_missing_mandatory_fields() {
 
+
+//log file entry
+        LoggerLoad.info("Reading POST endpoint...");
+
+        String endpoint = ReadProperties.getProperty("user_PostEndpoint");
+
+        LoggerLoad.info("Making POST call for patient login...");
+
+
+        //Reading test data
+        JSONObject testData = readJson.setUpTestData();
+        JSONObject patientInfo = testData.getJSONObject(ReadProperties.getProperty("PatientLogin"));
+
+        // Convert patientInfo to JSON string
+        String patientInfoJson = "";
+
+        System.out.println(patientInfoJson);
+
+        // Send the POST request with form parameters
+        response = requestSpec
+                .body(patientInfoJson)
+                .post(endpoint);
+
+        // Log the response body
+        response.body().print();
+
     }
 
 
     @Given("User creates GET Request for the logout API with User endpoint")
     public void user_creates_GET_Request_for_the_logout_API_with_User_endpoint() {
 
+
+        //log file entry
+        LoggerLoad.info("Setting baseURI for RestAssured...");
+
+        //reading get url from properties file
+        String baseURL = ReadProperties.getProperty("baseURL");
+
+        //setting URI for RestAssured
+        RestAssured.baseURI = baseURL;
+
+
     }
 
 
+    @When("User sends HTTPS logout Request")
+    public void patient_logout(){
+
+        //log file entry
+        LoggerLoad.info("Reading GET endpoint...");
+
+        //reading GET endpoint from properties file
+        String endpoint = ReadProperties.getProperty("user_LogoutEndpoint");
+
+        //log file entry
+        LoggerLoad.info("Making Get all Patients call...");
+
+        //Making the GET call by passing the auth token
+        response = RestAssured.given()
+                .header("Authorization", "Bearer " + bearerToken)
+                .get(endpoint);
+
+
+    }
 
     @Then("User receives {int} OK Status with response  Logout successful")
-    public void user_receives_OK_Status_with_response_Logout_successful(Integer int1) {
+    public void user_receives_OK_Status_with_response_Logout_successful(int expectedStatusCode) {
+
+        LoggerLoad.info("Asserting response status code...");
+        assertEquals(expectedStatusCode, response.getStatusCode());
 
     }
 
